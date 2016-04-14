ff984091a1ec20f06fa6807c7586d21b9dc760f8
= <cali@calicastle.com>
2016-04-14 12:20:05 +0800

    Add 360 Link

---
+++ database/migrations/2016_04_14_115651_add_password_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPasswordColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('password', 10)->nullable();
+            $table->string('360_link')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('password');
+            $table->dropColumn('360_link');
+        });
+    }
+}
